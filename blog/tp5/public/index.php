9beed99cdf7c8584d6921a2c77974cf3dcdcbc74
hasee <565827433@qq.com>
2017-05-19 07:21:16 +0800

    edit something about build.php

--- blog/tp5/public/index.php
+++ blog/tp5/public/index.php
@@ -13,5 +13,11 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
+//开启调试模式
+define('APP_DEBUG',true);
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
+//读取自动生成定义文件
+$build = include APP_PATH.'build.php';
+//运行自动生成
+\think\Build::run($build);
