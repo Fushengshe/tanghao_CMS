833f4b3dd1ec02ab757e56b421aa3ecf10bad2b0
hasee <565827433@qq.com>
2017-05-22 00:28:34 +0800

    部分网页的链接

--- blog/tp5/application/admin/controller/Cate.php
+++ blog/tp5/application/admin/controller/Cate.php
@@ -1,10 +1,52 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+
 class Cate extends Controller
 {
     public function lst()
     {
+        $cateres = \think\Db::name('cate')->select();
+        $this->assign('cateres',$cateres);
         return $this -> fetch();
+    }
+    public function add()
+    {
+        if(request()->isPost()){
+            $data = [
+                'catename' => input('catename'),
+                'keywords' => input('keywords'),
+                'lmdesc' => input('lmdesc'),
+                'type' => input('type') ? input('type') : 0,
+                ];
+            $validate = \think\Loader::validate('Cate');
+            if ($validate->check($data)) {
+                $db = \think\Db::name('cate')->insert($data);
+                if ($db) {
+                    return $this->success('添加栏目成功', 'lst');
+                } else {
+                    return $this->error('添加栏目失败');
+                }
+            }else{
+                return $this->error($validate->getError());
+            }
+
+            return;
+        }
+        return $this -> fetch();
+    }
+    public function edit(){
+        $id = input('id');
+        $cates = db('cate')->where('id',$id)->find();
+        $this->assign('cates',$cates);
+        return $this -> fetch();
+    }
+    public function del(){
+        $id = input('id');
+        if(db('cate')->delete($id)){
+            return $this->success('删除栏目成功！','lst');
+        }else{
+            return $this->error('删除栏目失败！');
+        }
     }
 }
---
+++ blog/tp5/application/admin/validate/Cate.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\admin\validate;
+
+use think\Validate;
+
+class Cate extends Validate
+{
+    protected $rule = [
+        'catename' => 'require|max:25',
+        'catename'   => 'unique:cate',
+//        'email' => 'email',
+    ];
+    protected $message = [
+        'catename.require'  => '栏目名称不能为空！',
+        'catename.unique'  => '栏目名称不能重复！',
+//        'name.max'      => '名称最多不能超过25个字符',
+//        'age.number'    => '年龄必须是数字',
+//        'age.between'   => '年龄只能在1-120之间',
+//        'email'         => '邮箱格式错误',
+    ];
+
+
+}
