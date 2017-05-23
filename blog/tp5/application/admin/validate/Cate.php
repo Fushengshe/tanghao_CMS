4cb36ade386ea91c82739ad73bf01ec4c38261ad
hasee <565827433@qq.com>
2017-05-23 16:55:12 +0800

    部分验证功能

--- blog/tp5/application/admin/validate/Cate.php
+++ blog/tp5/application/admin/validate/Cate.php
@@ -6,13 +6,13 @@ use think\Validate;
 class Cate extends Validate
 {
     protected $rule = [
-        'catename' => 'require|max:25',
-        'catename'   => 'unique:cate',
+        'catename' => 'require|max:25|unique:cate',
 //        'email' => 'email',
     ];
     protected $message = [
         'catename.require'  => '栏目名称不能为空！',
         'catename.unique'  => '栏目名称不能重复！',
+        'catename.max'  => '栏目名称不能超过25位！',
 //        'name.max'      => '名称最多不能超过25个字符',
 //        'age.number'    => '年龄必须是数字',
 //        'age.between'   => '年龄只能在1-120之间',
