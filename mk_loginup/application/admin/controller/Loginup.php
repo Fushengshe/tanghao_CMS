3e5dee3f00dd0ee2f4289e780693d02d92c6237a
hasee <565827433@qq.com>
2017-06-17 22:33:45 +0800

    加上了电话号码

--- mk_loginup/application/admin/controller/Loginup.php
+++ mk_loginup/application/admin/controller/Loginup.php
@@ -9,11 +9,11 @@ class Loginup extends Controller
 //        $links = \think\Db::name('link')->paginate(3);
 //        $this->assign('links', $links);
         if(request()->isPost()){
-            $login = new Log;
+            $loginup = new Log;
             $username = input('username');
             $password = input('password');
-            $status = $login->loginup($username,$password);
-
+            $mobile = input('mobile');
+            $status = $loginup->loginup($username,$password,$mobile);
             if($status == 1){
                 $intermediateSalt = uniqid(rand(), true);
                 $salt = substr($intermediateSalt, 0, 4);
@@ -22,21 +22,24 @@ class Loginup extends Controller
                 $data = [
                     'username' => $username,
                     'password' => $password,
+                    'mobile' => $mobile,
                     'usergroup' => 1,
-                    'token' => '',
-                    'token_exp' => 3600,
                     'created_at' => date("Y-m-d"),
                     'updated_at' => date("Y-m-d"),
                     'salt' => $salt,
                 ];
                 \think\Db::table('mk_user')->insert($data);
                 return $this->success('注册成功！');
-            }elseif($status == 4){
+            }elseif($status == 5){
                 return $this->error('注册失败，用户名已存在！');
-            }elseif($status == 3){
+            }elseif($status == 4){
                 return $this->error('注册失败，用户名不能为空！');
-            }elseif($status == 2){
+            }elseif($status ==3){
                 return $this->error('注册失败，密码不能为空！');
+            }elseif($status ==2){
+                return $this->error('注册失败，手机号不能为空！');
+            }elseif($status ==22){
+                return $this->error('注册失败，手机号必须为数字！！');
             }else{
                 return $this->error('注册失败!');
             }
--- mk_loginup/application/admin/model/Loginup.php
+++ mk_loginup/application/admin/model/Loginup.php
@@ -3,14 +3,18 @@ namespace app\admin\model;
 use think\Model;
 
 class Loginup extends Model{
-    public function loginup($username,$password){
+    public function loginup($username,$password,$mobile){
         $admin  = \think\Db::name('user')->where('username','=',$username)->find();
         if($admin){
-            return 4;
+            return 5;
         }elseif($username == ''){
-            return 3;
+            return 4;
         }elseif($password == ''){
+            return 3;
+        }elseif($mobile == ''){
             return 2;
+        }elseif(is_int($mobile)){
+            return 22;
         }else{
             return 1;
         }
